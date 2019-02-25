8a30529432e7fe3d6c032f8232ee2f61dd4cfe01
Jeffrey Kaplan (JK514-PC) <jeffmankap@gmail.com>
2019-02-25 07:50:07 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const port = process.env.PORT || 3000;
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view enginge', 'hbs');
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+
+  var log = `${now}: ${req.method} ${req.url}`;
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('unable to access log');
+    }
+  });
+  next();
+});
+
+// maintenance site-screen
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'My Homepage',
+    welcomeMessage: "Welcome to my website. Smiley face."
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'request did not work'
+  });
+});
+
+// set up a port to listen for get requests
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
